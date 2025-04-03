565e3b3ac662f4037c5fb881ab608065f2ff6763
Mark Elston <Mark.Elston@advantest.com>
2025-04-03 19:56:08 -0400

    Cleaned up some convention issues

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -15,7 +15,6 @@ fun isRunningLocally() : Boolean {
 
 fun getConnection(): Connection? {
 
-    val localIP = getLocalIpAddress()
     val hostAddr = if (isRunningLocally()) "localhost" else "192.168.1.175"
 
     val jdbcUrl = "jdbc:mysql://${hostAddr}:3306/bookdb"
@@ -30,21 +29,20 @@ fun getConnection(): Connection? {
     }
 }
 
-data class BookRecord(val id: Int, var title: String, var publisher_id: String, var file_path: String,
-                      var is_read: Boolean, var is_favorite: Boolean) {
+data class BookRecord(val id: Int, var title: String, var publisherId: String, var filePath: String,
+                      var isRead: Boolean, var isFavorite: Boolean) {
     constructor(rs: ResultSet) : this(rs.getInt("id"), rs.getString("title"), rs.getString("publisher_id"),
-            rs.getString("file_path"), rs.getBoolean("is_read"), rs.getBoolean("is_favorite")) {
-    }
+            rs.getString("file_path"), rs.getBoolean("is_read"), rs.getBoolean("is_favorite"))
 }
 
-fun insertRecord(connection: Connection) {
-    val sql = "INSERT INTO users (name, email) VALUES (?, ?)"
-    val preparedStatement = connection.prepareStatement(sql)
-    preparedStatement.setString(1, "John Doe")
-    preparedStatement.setString(2, "john@example.com")
-    val row = preparedStatement.executeUpdate()
-    println("$row row(s) inserted.")
-}
+//fun insertRecord(connection: Connection) {
+//    val sql = "INSERT INTO users (name, email) VALUES (?, ?)"
+//    val preparedStatement = connection.prepareStatement(sql)
+//    preparedStatement.setString(1, "John Doe")
+//    preparedStatement.setString(2, "john@example.com")
+//    val row = preparedStatement.executeUpdate()
+//    println("$row row(s) inserted.")
+//}
 
 fun readRecords(connection: Connection) {
     val sql = "SELECT * FROM books ORDER BY publisher_id"
@@ -53,32 +51,32 @@ fun readRecords(connection: Connection) {
 
     while (resultSet.next()) {
         val book = BookRecord(resultSet)
-        val pubID = String.format("%8s", book.publisher_id)
+        val pubID = String.format("%8s", book.publisherId)
         val title = String.format("%15s", book.title)
         val msg = """
-            |${pubID} - ${title} (${book.file_path})
-            |           ${book.is_read}/${book.is_favorite}
+            |${pubID} - $title (${book.filePath})
+            |           ${book.isRead}/${book.isFavorite}
             """.trimIndent()
         println(msg)
     }
 }
 
-fun updateRecord(connection: Connection) {
-    val sql = "UPDATE users SET email = ? WHERE name = ?"
-    val preparedStatement = connection.prepareStatement(sql)
-    preparedStatement.setString(1, "new-email@example.com")
-    preparedStatement.setString(2, "John Doe")
-    val row = preparedStatement.executeUpdate()
-    println("$row row(s) updated.")
-}
+//fun updateRecord(connection: Connection) {
+//    val sql = "UPDATE users SET email = ? WHERE name = ?"
+//    val preparedStatement = connection.prepareStatement(sql)
+//    preparedStatement.setString(1, "new-email@example.com")
+//    preparedStatement.setString(2, "John Doe")
+//    val row = preparedStatement.executeUpdate()
+//    println("$row row(s) updated.")
+//}
 
-fun deleteRecord(connection: Connection) {
-    val sql = "DELETE FROM users WHERE name = ?"
-    val preparedStatement = connection.prepareStatement(sql)
-    preparedStatement.setString(1, "John Doe")
-    val row = preparedStatement.executeUpdate()
-    println("$row row(s) deleted.")
-}
+//fun deleteRecord(connection: Connection) {
+//    val sql = "DELETE FROM users WHERE name = ?"
+//    val preparedStatement = connection.prepareStatement(sql)
+//    preparedStatement.setString(1, "John Doe")
+//    val row = preparedStatement.executeUpdate()
+//    println("$row row(s) deleted.")
+//}
 
 fun main() {
     println("Local Address = " + getLocalIpAddress())
