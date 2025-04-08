fea0d5733d9c2434e14e5b3f5edb557800a1f063
Mark Elston <melston1777@gmail.com>
2025-04-08 13:48:26 -0400

    make scripts work with kotlin > 2.0
    script filenames have to end in .main.kts

--- scripts/simpleScript.main.kts
+++ scripts/simpleScript.main.kts
@@ -1,3 +1,6 @@
+@file:Repository("https://repo1.maven.org/maven2")
+@file:DependsOn("mysql:mysql-connector-java:8.0.28")
+@file:DependsOn("build/libs/BookDBKotlin-1.0-SNAPSHOT.jar")
 
 import org.elsoft.dbops.BookRecord
 
